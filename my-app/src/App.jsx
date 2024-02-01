311ecccc67cceffef5194cffdb4a5f43956c2402
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-02-01 14:36:57 +0545

    loginpage added

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+
+import React from "react";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components/routes can be added here */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
